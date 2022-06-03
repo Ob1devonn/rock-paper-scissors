a394087dc96c048a891bbad6d5fe2a6886c275a1
ob1devonn <ob1devonn@gmail.com>
2022-06-03 16:54:36 -0600

    Rewrite and organize functions and add some This will update functions to simplify code. Add a function to determine victor set a list for winners to be displayed.

---
+++ script.js
@@ -0,0 +1,46 @@
+
+const winners = [];
+const list = ['rock', 'paper', 'scissors'];
+function playRound(playerSelection, computerSelection){
+    //check winner and store for later
+    const winner = checkWinner(playerSelection, computerSelection);
+    playerSelection = playerChoice();
+    computerSelection = computerChoice();
+    console.log(computerSelection,' comp');
+    console.log(playerSelection, ' player');
+}
+
+function game(){
+    playRound();
+}
+//create random selector for computer
+function computerChoice(){
+    return list[Math.floor(Math.random()*list.length)];
+}
+//get and validate player's choice
+function playerChoice(input){
+    input = prompt('Type Rock, Paper, or Scissors');
+    input = input.toLowerCase();
+    validate = validateX(input);
+    while(validate === false){
+        return playerChoice(input);
+    }
+    return input;
+}
+
+
+//validate player input
+function validateX(x){
+    return list.includes(x.toLowerCase());
+}
+//check for the round winner
+function checkWinner(x, y){
+    (x == y) ? 'Tie':
+    //list x winning conditions
+    ((x == 'rock' && y == 'scissors')||
+    (x == 'paper' && y == 'rock')||
+    (x == 'scissors' && y == 'paper')) ? 'Player' :
+    'Computer';
+}
+
+game();
