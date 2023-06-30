7ad23cf3624917fb19afc42dd3f313532ed62057
DeVonn Cluse <ob1devonn@gmail.com>
2023-06-30 14:27:44 -0600

    Simplify code on external script

--- script.js
+++ script.js
@@ -1,90 +1,52 @@
+const wins = [];
 
-const winners = [];
-const list = ['rock', 'paper', 'scissors'];
-
-
-function playRound(round){
-    const playerSelection = playerChoice();
-    const computerSelection = computerChoice();
-    //check winner and store for later
-    const winner = checkWinner(playerSelection, computerSelection);
-    //push round winner to winner[]
-    winners.push(winner);
-    console.log(winners);
-    
-    //write a function for logging the winner of each round and display who won game
-    logRound(playerSelection, computerSelection, winner, round);
-    console.log('---------------------------------------------');
-}
-
-function game(i){
-    for(i = 1; i < 6; i++){
-    playRound(i);
-    }//Write a function for displaying score at end
-    logWins();
-}
-//create random selector for computer
-function computerChoice(){
-    return list[Math.floor(Math.random()*list.length)];
-}
-//get and validate player's choice
-function playerChoice(){
-    let input = prompt('Type Rock, Paper, or Scissors');
-    if(input === null){
-        return playerChoice();
-    }
-    input = input.toLowerCase();
-    let validate = validateX(input);
-    while(validate === false){ 
-        return validate = playerChoice();
-    }
-    return input;
-}
-
-
-//validate player input
-function validateX(x){
-    return list.includes(x.toLowerCase());
-}
-//check for the round winner
-function checkWinner(x, y){
-    victor =
-    ((x == y) ? 'Tie':
-    //list x winning conditions
-    ((x == 'rock' && y == 'scissors')||
-    (x == 'paper' && y == 'rock')||
-    (x == 'scissors' && y == 'paper')) ?  'Player' :
-    'Computer');
-    return victor;
-}
-
-function logWins(){
-    //use output values from winners[] to filter individual scores
-    let playerWins = winners.filter((item) => item =='Player').length;
-    let computerWins = winners.filter((item) => item =='Computer').length;
-    let ties = winners.filter((item) => item =='Tie').length;
-    let gameWonBy = gameWinner(playerWins, computerWins, ties);
-    console.log('Results are:');
-    console.log('Your wins: ', playerWins);
-    console.log('Computer wins: ',computerWins);
-    console.log('Ties: ',ties);
-    console.log(gameWonBy);
-
-}
-
-function logRound(playerSelection, computerSelection, winner, round){
-    console.log('Round: ', round);
-    console.log('Player picked: ',playerSelection);
-    console.log('Computer picked: ', computerSelection);
-    console.log(winner, ': Won this round');
+function getComputerChoice(){
+    let choices = ['rock', 'paper', 'scissors'];
+    let choice = choices[Math.floor(Math.random() * 3)];
+    return choice;
     
 }
-//Function for overall winner
-function gameWinner(playerWins, computerWins, ties){
-     let wonBy = 
-    (playerWins > computerWins && playerWins > ties) ? 'Player Wins the Game!':
-    (computerWins > playerWins && computerWins > ties) ? 'Computer Wins the Game!':
-    'It was a TIE';
-   return wonBy;
+
+
+function playRound(userChoice, computerChoice){
+    if(userChoice === computerChoice){
+        return "It's a tie!";
+    }else if (
+        (userChoice === "rock" && computerChoice === "scissors") ||
+        (userChoice === "paper" && computerChoice === "rock") ||
+        (userChoice === "scissors" && computerChoice === "paper")
+        ){
+            return `You win!`;
+        }else {
+            return `Computer Wins!`;
+        } 
+    }
+    
+function game(){
+    let userScore = 0;
+    let computerScore = 0;
+    for(let round = 1; round <= 5; round++){
+        const computerSelection = getComputerChoice();
+        const playerSelection = prompt(`Round ${round} : Choose Rock, Paper, or Scissors`).toLowerCase();
+        
+        const result = playRound(playerSelection, computerSelection);
+        console.log(`Round ${round} : ${result}`);
+        console.log('---------------------------------------------------');
+        if(result === 'You win!'){
+            userScore++;
+        }else if (result === 'Computer Wins!'){
+            computerScore++;
+        }
+    }
+
+    console.log(`Final Score: You:${userScore} Computer:${computerScore}`);
+    if(userScore > computerScore){
+        console.log("You win the game!");
+    } else if(computerScore > userScore){
+        console.log("Computer wins the game!");
+    } else{
+        console.log ("The game was a Tie!!!");
+    }
 }
+
 game();
