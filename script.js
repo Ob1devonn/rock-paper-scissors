e023d3f9d791bd9e13faef2281170cee907511d4
DeVonn <ob1devonn@gmail.com>
2023-08-08 19:31:12 -0600

    Finish Script

--- script.js
+++ script.js
@@ -27,3 +27,45 @@ function playRound(playerSelection, computerSelection) {
     return "Computer wins!";
   }
 }
+
+//create a function to update results
+function updateResults(result) {
+  const resultDiv = document.getElementById("results");
+  resultDiv.textContent = `Player: ${playerScore} - Computer: ${computerScore} - ${result}`;
+}
+
+//target and write the winner to DOM
+function updateWinner() {
+  const winnerDiv = document.getElementById("winner");
+  if (roundsPlayed === 5) {
+    if (playerScore > computerScore) {
+      winnerDiv.textContent = "Congratulations! You won the game!";
+    } else if (computerScore > playerScore) {
+      winnerDiv.textContent = "The computer wins the game!";
+    } else {
+      winnerDiv.textContent = "The game is Tied! Everyone is a winner!";
+    }
+  }
+}
+
+//Listen for button clicks to determine playerChoice
+document.querySelectorAll(".choice").forEach((choice) => {
+  //for each choice attach eventlistener
+  choice.addEventListener("click", () => {
+    if (roundsPlayed < 5) {
+      // on click set playerSelection to the clicked on button id
+      const playerSelection = choice.id;
+      //get computerSelection
+      const computerSelection = computerChoice();
+      //play a round and store results
+      const result = playRound(playerSelection, computerSelection);
+      //place the results on display
+      updateResults(result);
+
+      roundsPlayed++;
+      if (roundsPlayed === 5) {
+        updateWinner();
+      }
+    }
+  });
+});
